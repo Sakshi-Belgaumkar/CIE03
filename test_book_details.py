071053a8e420ae1dfa650bc3d18c3dccd4b3d8a7
Sakshi-Belgaumkar <sakshibelgaumkar04@gmail.com>
2026-01-08 15:38:56 +0530

    Update test_book_details.py

--- test_book_details.py
+++ test_book_details.py
@@ -2,9 +2,9 @@ from book_details import book_details
 
 def test_book_details():
     book_id = 101
-    book_title = "The Alchemist"
-    author_name = "Paulo Coelho"
-    year_of_pub = 2016
+    book_title = "Ikigai"
+    author_name = "XYZ"
+    year_of_pub = 2019
 
     expected = (
         "Book ID : 102\n"
